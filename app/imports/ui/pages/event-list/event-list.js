7a238e11792a0e5dcda764074d18e16ea7c08a85
tylergdorn <31430148+tylergdorn@users.noreply.github.com>
2017-12-11 11:14:24 -1000

    whoops v2
    I didn't change the name of the helpers

--- app/imports/ui/pages/event-list/event-list.js
+++ app/imports/ui/pages/event-list/event-list.js
@@ -2,15 +2,13 @@ import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { Events } from '/imports/api/event/EventCollection';
 
-Template.Filter_Page.onCreated(function onCreated() {
+Template.Event_List.onCreated(function onCreated() {
   this.subscribe(Events.getPublicationName());
   this.messageFlags = new ReactiveDict();
 });
 
-Template.Filter_Page.helpers({
-  eventss() {
-    // Find all profiles with the currently selected interests.
-    const allEvents = Events.findAll();
-    return allEvents;
+Template.Event_List.helpers({
+  events() {
+    return Events.findAll();
   },
 });

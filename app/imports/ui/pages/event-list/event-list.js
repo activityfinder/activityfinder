4b2c06ba83c9c498f2a634229d38270b96295a90
tylergdorn <31430148+tylergdorn@users.noreply.github.com>
2017-12-11 11:02:47 -1000

    clean this up

--- app/imports/ui/pages/event-list/event-list.js
+++ app/imports/ui/pages/event-list/event-list.js
@@ -1,27 +1,16 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
-import { _ } from 'meteor/underscore';
 import { Events } from '/imports/api/event/EventCollection';
-import { Interests } from '/imports/api/interest/InterestCollection';
-
-const selectedInterestsKey = 'selectedInterests';
 
 Template.Filter_Page.onCreated(function onCreated() {
-  this.subscribe(Interests.getPublicationName());
   this.subscribe(Events.getPublicationName());
   this.messageFlags = new ReactiveDict();
-  this.messageFlags.set(selectedInterestsKey, undefined);
 });
 
 Template.Filter_Page.helpers({
-  events() {
-    // Initialize selectedInterests to all of them if messageFlags is undefined.
-    if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
-      Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
-    }
+  eventss() {
     // Find all profiles with the currently selected interests.
     const allEvents = Events.findAll();
-    const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
-    return _.filter(allEvents, eventss => _.intersection(eventss.interests, selectedInterests).length > 0);
+    return allEvents;
   },
 });

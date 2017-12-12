0f93fa8f3e11618dfbdf35575c49000c0fe27346
joshducey <31464793+joshducey@users.noreply.github.com>
2017-12-12 10:29:14 -1000

    join event prob

---
+++ app/imports/ui/components/event-list/event-card.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import { Events } from '/imports/api/event/EventCollection';
+
+Template.Event_Card.events({
+  'click .join-event' (idk) {
+
+  },
+});
